bba0c307ed16412273ac8570c1f8fb00cb6fea27
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-09-03 22:33:05 -0400

    updated server.js file

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var expressHandlebars = require("express-handlebars")
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+var request = require("request");
+var cheerio = require("cheerio");
